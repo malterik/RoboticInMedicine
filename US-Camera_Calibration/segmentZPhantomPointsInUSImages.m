875987300a8479aa3ee1363dccddf91a302e3f3b
matsch28 <m.slu@t-online.de>
2015-06-09 16:26:14 +0200

    impl

--- US-Camera_Calibration/segmentZPhantomPointsInUSImages.m
+++ US-Camera_Calibration/segmentZPhantomPointsInUSImages.m
@@ -1,3 +1,128 @@
+<<<<<<< HEAD
+function [c1, c2, c3, xmmPerPx, ymmPerPx, allImages] = segmentZPhantomPointsInUSImages(filenamePref, numImages)
+%SEGMENTZPHANTOMPOINTSINUSIMAGES Segments the Ultrasound images
+%   SEGMENTZPHANTOMPOINTSINUSIMAGES(FILENAMEPREF, NUMIMAGES)
+%   Reads in NUMIMAGES images with the prefix FILENAMEPREF and outputs the
+%   locations of the cross-sectional points of z-wire phantom.
+%
+%   Modified 22th May 2015
+%   Omer Rajput - MTEC, TUHH.
+
+% Read images
+I = imread([filenamePref '0.jpg']);
+
+% These are just example of expected segmentation results. You would
+% eventually not need the following two lines.
+%filenamePrefSplit = strsplit(filenamePref, {'/','\'});
+
+%filenameSegPref = [strjoin(filenamePrefSplit(1:end-1),'\') '\seg\seg'];
+%ISeg = imread([filenameSegPref '0.jpg']);
+
+% creating a figure with dummy structure
+%scrsz = get(groot,'ScreenSize');
+%figure('Position',[10 scrsz(4)*3/4 scrsz(3)*3/4 scrsz(4)*3/4]);
+
+%subplot(1,2,1)
+%ims = imshow(I);
+%title('Image from Ultrasound device')
+
+%subplot(1,2,2)
+%ims1 = imshow(ISeg);
+%title('Segmented z-wire cross-sectional points')
+
+sz = size(I);
+allImages = uint8(zeros(sz(1),sz(2),numImages));
+
+c1 = zeros(numImages,2);
+c2 = zeros(numImages,2);
+c3 = zeros(numImages,2);
+
+xmmPerPxAll = zeros(1,numImages);
+ymmPerPx = 40.0 / 480; % known
+
+fhResult = figure;
+
+for idx = 0:numImages-1
+    %delete(ims); delete(ims1);
+    disp(['Segmentation of image.. ' num2str(idx)]);
+    % Reading the image
+    allImages(:,:,idx+1) = imread([filenamePref num2str(idx) '.jpg']);
+    I = allImages(:,:,idx+1);
+    
+    %ISeg = imread([filenameSegPref num2str(idx) '.jpg']);
+    % Showing the US image
+    
+    %fhComp = figure;
+    %subplot(1,2,1);
+    %imshow(I)
+    %title('Image from Ultrasound device')
+    %
+    % Implement here the segmentation
+    % ...
+    %
+    
+    % define ROI containing the 3 points
+    fhRoi = figure;
+    imshow(I);
+    mask = roipoly;
+    close(fhRoi);
+    I(~mask) = 0;
+    % binarize
+    th = graythresh(I);
+    I = im2bw(I,th);
+    % detect connected regions
+    CC = bwconncomp(I,4);
+    if CC.NumObjects ~= 3
+        error(['Detected ',num2str(CC.NumObjects),' objects! Discarding image.']);
+    end;
+    % compute center of the connected regions
+    c = zeros(3,2);
+    for i = 1:3
+        [row,col] = ind2sub(sz,CC.PixelIdxList{i});
+        c(i,:) = [mean(row), mean(col)];
+    end;
+    c1(idx+1,:) = c(1,:);
+    c2(idx+1,:) = c(2,:);
+    c3(idx+1,:) = c(3,:);
+    
+    %figure(fhComp);
+    %subplot(1,2,1);
+    %hold on;
+    %plot(c(:,2),c(:,1),'r+');
+    
+    % Showing the original thresholded image with centroids
+    %subplot(1,2,2);
+    %imshow(ISeg)
+    %title('Expected segmentation results')
+    
+    figure(fhResult);
+    imshow(allImages(:,:,idx+1));
+    hold on;
+    plot(c(1,2),c(1,1),'r+');
+    plot(c(2,2),c(2,1),'g+');
+    plot(c(3,2),c(3,1),'c+');
+    
+    % It is also helpful to estimate mm per pixel
+    % Counting the white pixels in each column, to find out the width in
+    % pixels. This will correspond to 38mm (from the probe).
+    sumRow = sum(I,1);
+    indF = find(sumRow, 1, 'first');
+    indL = find(sumRow, 1, 'last');
+    widthPxUS = indL - indF + 1;
+    xmmPerPxAll(idx+1) = 38.0 / widthPxUS;
+    pause(0.1);
+end
+
+xmmPerPx = mean(xmmPerPxAll);
+
+close(fhResult);
+
+% Outputting all zeros - please modify it based on the segmentation above.
+%c1 = zeros(numImages,2);
+%c2 = zeros(numImages,2);
+%c3 = zeros(numImages,2);
+
+=======
 function [c1, c2, c3, xmmPerPx, ymmPerPx, allImages] = segmentZPhantomPointsInUSImages(filenamePref, numImages)
 %SEGMENTZPHANTOMPOINTSINUSIMAGES Segments the Ultrasound images
 %   SEGMENTZPHANTOMPOINTSINUSIMAGES(FILENAMEPREF, NUMIMAGES)
@@ -73,4 +198,5 @@ c1 = zeros(numImages,2);
 c2 = zeros(numImages,2);
 c3 = zeros(numImages,2);
 
+>>>>>>> origin/master
 end
--- US-Camera_Calibration/zWirePhantomInCameraCoord.m
+++ US-Camera_Calibration/zWirePhantomInCameraCoord.m
@@ -1,3 +1,103 @@
+<<<<<<< HEAD
+function [p, TAll] = zWirePhantomInCameraCoord(camServerIP, numRepition)
+%ZWIREPHANTOMINCAMERACOORD Connects to the "CambarServer"
+%   ZWIREPHANTOMINCAMERACOORD(CAMSERVERIP)
+%   Connects to the "CambarServer" with IP address of CAMSERVERIP at port
+%   3000.
+%
+%   ZWIREPHANTOMINCAMERACOORD(IPSTRVAR,NUMREPITION) Connects to the
+%   "CambarServer" with with IP address of CAMSERVERIP at port
+%   3000. It repeats the measurement NUMREPITION times (Default: 5).
+%
+%   Returns the coordinates of the tip of stylus-locator in 'p' and the
+%   transformations of the stylus-locator in 'TAll' relative to the camera 
+%   coordinate system. 
+% 
+%   This function assumes that the Cambar Tracking has been trained to
+%   track the 'stylus' locator. The 'stylus' is a custom 3D-printed object
+%   with four reflective markers located on it. 
+%
+%   The CambarServer should be able to find the 'stylus'-locator file at 
+%   'C:\locators\' in the PC where CambarServer is running.
+%   
+%   Modified 22th May 2015
+%   Omer Rajput - MTEC, TUHH.
+
+% handle input arguments
+if nargin == 1
+    numRepition = 5;
+elseif nargin > 2 && nargin == 0
+    error('err:zWirePhantomInCameraCoord:wrongArguments', ...
+        ['There was an unsupported number of input arguments\n',...
+         'Usage: zWirePhantomInCameraCoord(''134.28.45.63'')', ...
+         '   or: zWirePhantomInCameraCoord(''134.28.45.63'', 5)' ]);
+end
+
+camServerPort = 3000;
+
+% Open the TCP/IP-Connection
+disp('Connecting to Cambar Server')
+jTcpObj = jtcp('request', camServerIP, camServerPort,'serialize',false);
+pause(0.1);
+mssg = char(jtcp('read',jTcpObj)); disp(mssg)
+
+% Send the keyword mtec to authorize the client
+jtcp('write',jTcpObj,int8('mtec')); pause(0.1);
+mssg = char(jtcp('read',jTcpObj)); disp(mssg);
+
+disp('Loading Stylus locator')
+% Load locator definition file stylus.xml from the config folder
+% The locator must be located on the camera-pc in the folder
+% C:\locators\
+jtcp('write',jTcpObj,int8('LoadLocator stylus')); pause(0.1);
+mssg = char(jtcp('read',jTcpObj)); disp(mssg);
+
+jtcp('write',jTcpObj,int8('LoadLocator stylus')); pause(0.1);
+mssg = char(jtcp('read',jTcpObj)); disp(mssg);
+
+disp('Starting the measurements...');
+disp(['Measure the four points of the phantom ', num2str(numRepition),...
+    ' times in the same order.']);
+disp(['Make sure that you have correctly placed the tip of the stylus',...
+     ' at the location you want to measure.']);
+disp('Press any key when you are ready for the first measurement...');
+% A loop to aquire data continously
+N = 4; % for 4 wire-end-points on the z-shape.
+figure;hold on;pause;
+p = zeros(4,N*numRepition);
+TAll = zeros(16,N*numRepition);
+for rep = 1:numRepition
+    i=1;
+    while (i<=N)
+        % Send the command word to get the locator position. The locator has to
+        % be loaded before.
+        [T,timestamp] = GetLocatorTransformMatrix(t, 'stylus');
+        if (sum(T(:))~=1)
+            absPtNum = i+(rep-1)*N;
+            TAll(:,absPtNum) = T(:);
+            % transform marker center point in camera coordinates
+            p(:,absPtNum) = T * [0,0,0,1]';
+            disp(['Timestamp: ' num2str(timestamp) 'rep: ' num2str(rep) ', p' num2str(i) ': ' num2str(p(:,absPtNum)')])
+            % plot the marker position
+            plot3(p(1,absPtNum),p(2,absPtNum),p(3,absPtNum),'ko');
+            title(['rep: ' num2str(rep) ', measurements so far ' num2str(absPtNum)]);
+            % wait 0.001 seconds
+            disp('Press any key to continue to the next measurement...')
+            pause;
+            i=i+1;
+        else
+            disp('All zero T received. Try the same measurement again.')
+        end
+    end
+end
+hold off;
+
+prompt = 'Do you want to save the measured data for later use? Y/N [N]: ';
+str = input(prompt,'s');
+if strcmpi(str,'y')
+    mkdir('./','ultrasoundImagesAndPoses')
+    save('ultrasoundImagesAndPoses/zWireEndPoints.mw','p','TAll');
+=======
 function [p, TAll] = zWirePhantomInCameraCoord(camServerIP, numRepition)
 %ZWIREPHANTOMINCAMERACOORD Connects to the "CambarServer"
 %   ZWIREPHANTOMINCAMERACOORD(CAMSERVERIP)
@@ -94,4 +194,5 @@ str = input(prompt,'s');
 if strcmpi(str,'y')
     mkdir('./','ultrasoundImagesAndPoses')
     save('ultrasoundImagesAndPoses/zWireEndPoints.mw','p','TAll');
+>>>>>>> origin/master
 end
