0be4bd2a85fba62b8eb5c5ca0b73e1b80f3f8c13
matsch28 <m.slu@t-online.de>
2015-07-09 08:31:38 +0200

    fix

--- US-Camera_Calibration/segmentZPhantomPointsInUSImages.m
+++ US-Camera_Calibration/segmentZPhantomPointsInUSImages.m
@@ -69,7 +69,7 @@ for idx = 0:numImages-1
     c = zeros(3,2);
     for i = 1:3
         [row,col] = ind2sub(sz,CC.PixelIdxList{i});
-        c(i,:) = [mean(row), mean(col)];
+        c(i,:) = [mean(col), mean(row)];
     end;
     c1(idx+1,:) = c(1,:);
     c2(idx+1,:) = c(2,:);
@@ -79,9 +79,9 @@ for idx = 0:numImages-1
     figure(fhResult);
     imshow(allImages(:,:,idx+1));
     hold on;
-    plot(c(1,2),c(1,1),'r+');
-    plot(c(2,2),c(2,1),'g+');
-    plot(c(3,2),c(3,1),'c+');
+    plot(c(1,1),c(1,2),'r+');
+    plot(c(2,1),c(2,2),'g+');
+    plot(c(3,1),c(3,2),'c+');
     
     % It is also helpful to estimate mm per pixel
     % Counting the white pixels in each column, to find out the width in
